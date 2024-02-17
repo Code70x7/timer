e033f7b8bde8de1a047456ee269fb0332901d518
Tony Dayton <tonylynndayton@yahoo.com>
2024-02-17 12:09:01 -0700

    Update6

--- script.js
+++ script.js
@@ -35,4 +35,13 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton,
+    onStart() {
+    console.log('Timer started');
+},
+    onTick(){
+
+},
+    onComplete(){
+
+});

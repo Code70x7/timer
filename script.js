3e77de5b52549a801693676ea55968941bbaacad
Tony Dayton <tonylynndayton@yahoo.com>
2024-05-29 17:33:25 -0600

    Notes update

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
+//create DOM selectors for class use in constructor function
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 const circle = document.querySelector('circle');
+// const bodyColor = document.querySelector('body');
 
+// getting perimeter through math
 const perimeter = circle.getAttribute('r') * 2 * Math.PI;
 circle.setAttribute('stroke-dasharray', perimeter);
 
 let duration;
+//optional functions - instantiating new class
 const timer = new Timer(durationInput, startButton, pauseButton, {
     onStart(totalDuration) {
         duration = totalDuration;
     },
+    //creating math for dashoffset - animation of circle
     onTick(timeRemaining) {
         circle.setAttribute('stroke-dashoffset',
             perimeter * timeRemaining / duration - perimeter
